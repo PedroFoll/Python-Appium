1830d3a6e85cf31ade3b444716738af54e6578d5
Pedro Foll <pedro.fol29@gmail.com>
2022-03-07 16:30:44 -0300

    Deletando arquivos inuteis

--- Prototipos - Codigos antigos/gerador_telefone.py
+++
@@ -1,9 +0,0 @@
-from random import randint
-
-prefixo = (str(85))
-inicio = (str(99999))
-final = str((randint(1000,9999)))
-
-numeroCelularAleatorio = (prefixo+inicio+final)
-
-print(numeroCelularAleatorio)
